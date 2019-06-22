5378f84adad710e206411424039b9993284d4e5c
masayoshikuroda <33488154+masayoshikuroda@users.noreply.github.com>
2019-06-23 01:14:55 +0900

    Add files via upload

--- aconfig.py
+++ aconfig.py
@@ -1,7 +1,10 @@
 #coding:utf-8
 import sys
+import os
 import pyaudio
 import json
+
+ACONFIG_FILE = 'aconfig.json'
 
 sample_width = 1
 unsigned = False
@@ -9,17 +12,30 @@ channels = 1
 rate = 8000
 chunk = 512
 device_index = 0
-duration = -1;
+duration = -1
 
-with open('aconfig.json') as f:
-    config = json.load(f)
-    sample_width = config['sample_width']
-    unsigned = config['unsigned']
-    channels = config['channels']
-    rate = config['rate']
-    chunk = config['chunk']
-    device_index = config['device_index']
-    duration = config['duration']
+if not os.path.exists(ACONFIG_FILE):
+    config = {
+        "sample_width" : sample_width,
+      	"unsigned"     : unsigned,
+        "channels"     : channels,
+        "rate"         : rate,
+        "chunk"        : chunk,
+        "device_index" : device_index,
+        "duration"     : duration
+    }
+    with open(ACONFIG_FILE, 'w') as f:
+        json.dump(config, f)
+else:
+    with open(ACONFIG_FILE) as f:
+        config = json.load(f)
+        sample_width = config['sample_width']
+        unsigned = config['unsigned']
+        channels = config['channels']
+        rate = config['rate']
+        chunk = config['chunk']
+        device_index = config['device_index']
+        duration = config['duration']
 
 def get_count():
     return int(rate / chunk * duration)
